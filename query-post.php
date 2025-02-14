69ee74bc4b498cfc38c629d103ae290bbecc1ae0
MdShamimIslam <shamim401897@gmail.com>
2025-02-14 10:39:43 +0600

    created register post type and activation hook

--- query-post.php
+++ query-post.php
@@ -16,6 +16,9 @@ class MSI_Query_Post {
     private function __construct() {
         $this->define_constant(); 
         $this->load_classes(); 
+        // auto create post when the plugin is activated
+        register_activation_hook( __FILE__, [ $this, 'my_plugin_activate' ] );
+
     }
 
     public static function get_instance() {
@@ -33,11 +36,32 @@ class MSI_Query_Post {
     private function load_classes() {
         require_once MSI_PLUGIN_PATH . 'includes/Admin_Menu.php';
         require_once MSI_PLUGIN_PATH . 'includes/Custom_Column.php';
+        require_once MSI_PLUGIN_PATH . 'includes/Post_Type.php';
         // require_once MSI_PLUGIN_PATH . 'includes/PostType-Taxonomy.php';
 
         new MSI_Amin_Menu();
-        new \MSI\Custom_Column();
+        new MSI\Custom_Column();
+        new MSI\Post_Type();
         // new MSI\PostType_Taxonomy();
+    }
+
+    public function my_plugin_activate() {
+
+        $count_posts = get_posts([
+            'post_type' => 'book',
+            'fields' => 'ids'
+        ]);
+
+        if (count($count_posts) > 0) {
+           return;
+        }
+      
+        wp_insert_post( array(
+            'post_type' => 'book',
+            'post_title' => 'Auto Create Book',
+            'post_content' =>'All Knowledge here in a book',
+            'post_status' =>'publish'
+        ) );
     }
     
 }
